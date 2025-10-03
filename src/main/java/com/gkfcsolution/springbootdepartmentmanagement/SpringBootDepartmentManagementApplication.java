af14badfe371ddbb14705bda1c0eadad6f59bd4f
Frank GUEKENG - GKFC <frankguekeng11@gmail.com>
2025-10-03 15:26:53 +0100

    first commit

---
+++ src/main/java/com/gkfcsolution/springbootdepartmentmanagement/SpringBootDepartmentManagementApplication.java
@@ -0,0 +1,13 @@
+package com.gkfcsolution.springbootdepartmentmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDepartmentManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDepartmentManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gkfcsolution/springbootdepartmentmanagement/SpringBootDepartmentManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gkfcsolution.springbootdepartmentmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDepartmentManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
